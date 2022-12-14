c7c0fc65cd0b50097e939864af6bdcf8e6fa38ae
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-14 14:21:44 -0300

    Controllers added

---
+++ AppNoticias/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  AppNoticias
+//
+//  Created by Antonio Vuono on 13/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        // did code here
+        
+    }
+}
